a33889def62e2a23f8a254f2151a5eb9a419dcc6
NizarMjr <nizar.mjr94@gmail.com>
2024-01-25 20:32:14 +0100

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,5 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { initializeApp } from 'firebase/app'
-import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore';
 
 const Projects = () => {
     const color = useSelector(state => state.selectedColor)
@@ -12,33 +9,10 @@ const Projects = () => {
         getMyWorks()
     }, []);
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyAyt9OVC2u0qVXHLkWEzUTuvUXUykyDDG0",
-        authDomain: "fir-test-b6c87.firebaseapp.com",
-        projectId: "fir-test-b6c87",
-        storageBucket: "fir-test-b6c87.appspot.com",
-        messagingSenderId: "700344142572",
-        appId: "1:700344142572:web:6401b31cd9e1203932f3c9",
-        measurementId: "G-6J0KPWQX29"
-    };
-
     const getMyWorks = async () => {
-        try {
-            const app = initializeApp(firebaseConfig)
-            const db = getFirestore(app);
-            const colRef = collection(db, 'projects');
-
-            await getDocs(colRef).then((snapshot) => {
-                let works = [];
-                snapshot.docs.forEach((doc) => {
-                    works.push({ ...doc.data() })
-                })
-                setWorks(works)
-            })
-        }
-        catch (error) {
-            console.error("Firebase Error:", error);
-        }
+        const req = await fetch('./projects.json');
+        const res = await req.json();
+        setWorks(res);
     }
     return (
         <main id="portfolio" className="">
